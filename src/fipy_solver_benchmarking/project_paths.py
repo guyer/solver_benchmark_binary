95d93271f99c7c695e3e09c0d68646aee7a2793a
Jonathan Guyer <guyer@nist.gov>
2023-09-08 10:28:06 -0400

    Use project_paths to find files

--- src/fipy_solver_benchmarking/project_paths.py
+++ src/fipy_solver_benchmarking/project_paths.py
@@ -3,6 +3,6 @@
 from pathlib import Path
 
 module_path = Path(__file__)
-project_path = (module_path / "../../").resolve()
+project_path = (module_path / "../../../").resolve()
 data_path = (project_path / "data").resolve()
 results_path = (project_path / "results").resolve()
