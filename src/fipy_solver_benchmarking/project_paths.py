df522c3f5c0fee321b3755cd7c7698726b54c736
Jonathan Guyer <guyer@nist.gov>
2024-10-31 16:09:15 -0400

    Change package to workflow/rules

--- src/fipy_solver_benchmarking/project_paths.py
+++
@@ -1,9 +0,0 @@
-# Inspired by https://medium.com/analytics-vidhya/data-directory-in-jupyter-notebooks-dc46cd79eb2f
-
-from pathlib import Path
-
-module_path = Path(__file__)
-project_path = (module_path / "../../../").resolve()
-data_path = (project_path / "data").resolve()
-matrix_path = (data_path / "matrices").resolve()
-results_path = (project_path / "results").resolve()
