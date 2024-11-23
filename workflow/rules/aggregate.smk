3055658d715d1a83829ccbb84cf3940e9b7230d4
Jonathan Guyer <guyer@nist.gov>
2024-11-23 11:12:03 -0500

    Plot and report all benchmarks

--- workflow/rules/aggregate.smk
+++ workflow/rules/aggregate.smk
@@ -1,18 +1,22 @@
 rule plot_permutations:
     output:
-        total="results/plots/fipy~{rev}/total.png",
-        prepare="results/plots/fipy~{rev}/prepare.png",
-        solve="results/plots/fipy~{rev}/solve.png",
+        total="results/plots/fipy~{rev}/{benchmark}/total.png",
+        prepare="results/plots/fipy~{rev}/{benchmark}/prepare.png",
+        solve="results/plots/fipy~{rev}/{benchmark}/solve.png",
         plots=report(
-            directory("results/plots/fipy~{rev}"),
+            directory("results/plots/fipy~{rev}/{benchmark}"),
             patterns=["{name}.png"],
             category="{rev}",
-            labels={"name": "{name}"}
+            subcategory="{benchmark}",
+            labels={
+                "benchmark": "{benchmark}",
+                "name": "{name}"
+            }
         )
     input:
         "results/total_times.json"
     log:
-        "logs/plot_permutations_{rev}.log"
+        "logs/{benchmark}/plot_permutations_{rev}.log"
     conda:
         "../envs/snakemake.yml"
     script:
--- workflow/scripts/plot_permutations.py
+++ workflow/scripts/plot_permutations.py
@@ -187,20 +187,22 @@ def plot_sweep_times(df):
 
         plt.show()
 
-# https://stackoverflow.com/a/55849527/2019542
-logger = logging.getLogger('plot_permutations')
-fh = logging.FileHandler(str(snakemake.log[0]))
-fh.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-fh.setFormatter(formatter)
-logger.addHandler(fh)
+if __name__ == "__main__":
+    # https://stackoverflow.com/a/55849527/2019542
+    logger = logging.getLogger('plot_permutations')
+    fh = logging.FileHandler(str(snakemake.log[0]))
+    fh.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
 
-try:
-    df = pd.read_json(snakemake.input[0])
-    df = df.query(f"fipy_rev == '{snakemake.wildcards.rev}'")
-    plot_all(df, snakemake.output.total, ymin=1e-4, ymax=1e2)
-    plot_all(df, snakemake.output.prepare, data_set="prepare_seconds", ylabel="prepare time", ymin=1e-4, ymax=1e2)
-    plot_all(df, snakemake.output.solve, data_set="solve_seconds", ylabel="solve time", ymin=1e-4, ymax=1e2)
-except Exception as e:
-    logger.error(e, exc_info=True)
-    raise e
+    try:
+        df = pd.read_json(snakemake.input[0])
+        df = df.query(f"fipy_rev == '{snakemake.wildcards.rev}'"
+                      f"and benchmark == '{snakemake.wildcards.benchmark}'")
+        plot_all(df, snakemake.output.total, ymin=1e-4, ymax=1e2)
+        plot_all(df, snakemake.output.prepare, data_set="prepare_seconds", ylabel="prepare time", ymin=1e-4, ymax=1e2)
+        plot_all(df, snakemake.output.solve, data_set="solve_seconds", ylabel="solve time", ymin=1e-4, ymax=1e2)
+    except Exception as e:
+        logger.error(e, exc_info=True)
+        raise e
